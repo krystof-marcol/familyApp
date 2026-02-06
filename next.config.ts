ff6240ca17964789983d476114c8a9c8977e5b8a
Kryštof <156136292+Kikingod@users.noreply.github.com>
2026-02-06 12:22:24 +0100

    setuped service worker

--- next.config.ts
+++ next.config.ts
@@ -9,13 +9,51 @@ const withPWA = withPWAInit({
   register: true,
   disable: process.env.NODE_ENV === "development",
   workboxOptions: {
+    disableDevLogs: false,
     skipWaiting: true,
     clientsClaim: true,
+    runtimeCaching: [
+      {
+        // change url to match original
+        urlPattern: /.*\/api\/.*/i,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "api-data-cache",
+          expiration: {
+            maxEntries: 50,
+            maxAgeSeconds: 60 * 60 * 24,
+          },
+          networkTimeoutSeconds: 10,
+        },
+      },
+      {
+        urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+        handler: "CacheFirst",
+        options: {
+          cacheName: "google-fonts-cache",
+          expiration: {
+            maxEntries: 10,
+            maxAgeSeconds: 60 * 60 * 24 * 365,
+          },
+        },
+      },
+      {
+        urlPattern: /\/_next\/image\?url=.*/i,
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: "next-image-cache",
+          expiration: {
+            maxEntries: 64,
+            maxAgeSeconds: 60 * 60 * 24,
+          },
+        },
+      },
+    ],
   },
 });
 
 const nextConfig: NextConfig = {
-  /* your Next.js config options here */
+  // ... your other config
 };
 
 export default withPWA(withNextIntl(nextConfig));
