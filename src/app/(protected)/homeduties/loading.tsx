c487edd591a5f3a8377489d14d769f529799abec
Kryštof <156136292+Kikingod@users.noreply.github.com>
2026-02-17 20:17:46 +0100

    setup homeDutiesShell for loading in homeDuties

--- src/app/(protected)/homeduties/loading.tsx
+++ src/app/(protected)/homeduties/loading.tsx
@@ -1,25 +1,5 @@
-import { Skeleton } from "@/components/ui/skeleton";
+import HomeDutiesShell from "@/app/(protected)/homeduties/homeDutiesShell";
 
 export default function Loading() {
-  return (
-    <div className="flex flex-col h-full w-full p-4 space-y-4">
-      <div className="flex justify-between items-center">
-        <div className="space-y-2">
-          <Skeleton className="h-8 w-48" />
-          <Skeleton className="h-4 w-64" />
-        </div>
-        <Skeleton className="h-10 w-32" />
-      </div>
-
-      <div className="w-full h-px bg-gray-200 my-2" />
-
-      <div className="space-y-4">
-        {Array.from({ length: 6 }).map((_, i) => (
-          <div key={i} className="flex items-center space-x-4">
-            <Skeleton className="h-12 w-full rounded-xl" />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+  return <HomeDutiesShell />;
 }
